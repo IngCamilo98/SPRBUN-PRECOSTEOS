2cf5f328d3c544960edf69ff4eba81044a129c45
Hector Camilo Torres Diaz <hcamilotorresdiaz@gmail.com>
2026-01-03 10:11:26 -0500

    construccion basica del proyecto

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    # 1) Configuración / rutas base
+    # 2) Leer datos (Excel / BD)
+    # 3) Procesar (precosteo: filtrar, agrupar, totales, etc.)
+    # 4) Generar salida (PDF)
+    print('Hello world')
+
+if __name__ == "__main__":
+    main()
